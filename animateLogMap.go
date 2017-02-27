4b5be50a02ac19d2df16b8a00f03ecc92cd9709a
plantimals <sartorius.snouth@gmail.com>
2017-02-27 00:41:35 -0600

    functioning with stub driver

--- animateLogMap.go
+++ animateLogMap.go
@@ -1,4 +1,30 @@
 package main
 
-func NewLogisticMap() {
+import (
+	"image"
+	"image/gif"
+	"os"
+
+	"github.com/plantimals/logisticmap/logisticmap"
+)
+
+func main() {
+	lm := logisticmap.NewLogisticMap()
+	frames := 1
+	imgs := make([]*image.Paletted, frames)
+	delays := make([]int, frames)
+	start := float64(3.65)
+	stop := float64(3.75)
+	for i := 0; i < frames; i++ {
+		imgs[i] = lm.GetImage(start, stop, 0.00001)
+		delays[i] = 5
+		start += 0.01
+		stop -= 0.01
+	}
+	output, _ := os.OpenFile("test2.gif", os.O_WRONLY|os.O_CREATE, 0600)
+	defer output.Close()
+	gif.EncodeAll(output, &gif.GIF{
+		Image: imgs,
+		Delay: delays,
+	})
 }
--- logisticmap/logisticmap.go
+++ logisticmap/logisticmap.go
@@ -1,4 +1,4 @@
-package main
+package logisticmap
 
 import (
 	"image"
@@ -9,21 +9,11 @@ import (
 	"sync"
 )
 
-//var (
-//	BURN_IN = 100000
-//	TAKE    = 1000
-//	XSTEP   = float64(0.0001)
-//	XSTART  = float64(2.9)
-//	XSTOP   = float64(4.0)
-//	x_dim   int
-//	y_dim   int
-//)
-
-func newVSlice(idx int, param float64) *VSlice {
+func newVSlice(idx int, param float64, take int) *VSlice {
 	vSlice := new(VSlice)
 	vSlice.idx = idx
 	vSlice.param = param
-	vSlice.levels = make([]float64, TAKE)
+	vSlice.levels = make([]float64, take)
 	return vSlice
 }
 
@@ -52,22 +42,14 @@ type LogisicMap struct {
 const (
 	burnIn      = 100000
 	take        = 1000
-	step        = 0.0001
-	xStart      = float64(2.9)
-	xStop       = float64(4.0)
 	parallelism = 8
 )
 
 func NewLogisticMap() *LogisicMap {
-	lm = new(LogisicMap)
+	lm := new(LogisicMap)
 	lm.burnIn = burnIn
 	lm.take = take
-	lm.start = start
-	lm.stop = stop
-	lm.step = step
 
-	lm.x_dim = int(float64((XSTOP - XSTART)) / XSTEP)
-	lm.y_dim = int((x_dim * 3) / 4)
 	lm.parallelism = parallelism
 	return lm
 }
@@ -76,14 +58,18 @@ func (lm *LogisicMap) Parallelism(p int) {
 	lm.parallelism = p
 }
 
-func (lm *LogisicMap) GetFrame(writer io.Writer, start float64, stop float64, step float64) {
-
+func (lm *LogisicMap) GetGIF(writer io.Writer, start float64, stop float64, step float64) {
+	lm.start = start
+	lm.stop = stop
+	lm.step = step
+	lm.x_dim = int(float64((stop - start)) / step)
+	lm.y_dim = int((lm.x_dim * 3) / 4)
 	slices := make(map[int]*VSlice)
 	var fanout []<-chan *VSlice
 
-	regions := paramGen(start, stop, step)
+	regions := paramGen(start, stop, step, lm.take)
 
-	for i := 0; i < parallelism; i++ {
+	for i := 0; i < lm.parallelism; i++ {
 		fanout = append(fanout, iterateGen(regions, burnIn, take))
 	}
 	log.Printf("fanout size: %v", len(fanout))
@@ -93,7 +79,7 @@ func (lm *LogisicMap) GetFrame(writer io.Writer, start float64, stop float64, st
 
 	img := image.NewPaletted(image.Rect(0, 0, lm.x_dim, lm.y_dim), palette)
 	var images []*image.Paletted
-	fillImage(slices, img)
+	lm.fillImage(slices, img)
 	images = append(images, img)
 	gif.EncodeAll(writer, &gif.GIF{
 		Image: images,
@@ -101,9 +87,33 @@ func (lm *LogisicMap) GetFrame(writer io.Writer, start float64, stop float64, st
 	})
 }
 
-func fillImage(slices map[int]*VSlice, img *image.Paletted) {
-	yf := float64(y_dim)
-	for x := int(0); x < x_dim; x++ {
+func (lm *LogisicMap) GetImage(start float64, stop float64, step float64) *image.Paletted {
+	lm.start = start
+	lm.stop = stop
+	lm.step = step
+	lm.x_dim = int(float64((stop - start)) / step)
+	lm.y_dim = int((lm.x_dim * 3) / 4)
+	slices := make(map[int]*VSlice)
+	var fanout []<-chan *VSlice
+
+	regions := paramGen(start, stop, step, lm.take)
+
+	for i := 0; i < lm.parallelism; i++ {
+		fanout = append(fanout, iterateGen(regions, burnIn, take))
+	}
+	log.Printf("fanout size: %v", len(fanout))
+	for vslice := range fanin(fanout) {
+		slices[vslice.idx] = vslice
+	}
+
+	img := image.NewPaletted(image.Rect(0, 0, lm.x_dim, lm.y_dim), palette)
+	lm.fillImage(slices, img)
+	return img
+}
+
+func (lm *LogisicMap) fillImage(slices map[int]*VSlice, img *image.Paletted) {
+	yf := float64(lm.y_dim)
+	for x := int(0); x < lm.x_dim; x++ {
 		if _, ok := slices[x]; ok {
 			for _, p := range slices[x].levels {
 				y := int((1 - p) * yf)
@@ -139,13 +149,13 @@ func fanin(fanout []<-chan *VSlice) <-chan *VSlice {
 	return out
 }
 
-func paramGen(start float64, stop float64, step float64) <-chan *VSlice {
+func paramGen(start float64, stop float64, step float64, take int) <-chan *VSlice {
 	out := make(chan *VSlice)
 	go func() {
 		sliceCount := int((stop - start) / step)
 		for i := 0; i < sliceCount; i++ {
 			p := start + (float64(i) * step)
-			out <- newVSlice(i, p)
+			out <- newVSlice(i, p, take)
 		}
 		close(out)
 	}()
@@ -156,7 +166,7 @@ func iterateGen(in <-chan *VSlice, burnIn int, take int) <-chan *VSlice {
 	out := make(chan *VSlice)
 	go func() {
 		for vslice := range in {
-			iterate(vslice, brunIn, take)
+			iterate(vslice, burnIn, take)
 			out <- vslice
 		}
 		close(out)
